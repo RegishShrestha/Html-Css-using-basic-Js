0907bde9e68db22cec9276f81d44946432751967
RegishShrestha <87872185+RegishShrestha@users.noreply.github.com>
2022-01-18 16:20:16 +0545

    Add files via upload

---
+++ gym/js/main.js
@@ -0,0 +1,39 @@
+ // Initialize and add the map
+ function initMap() {
+    // Your location
+    const loc = { lat: 13.587180, lng: 79.315680 };
+    // Centered map on location
+    const map = new google.maps.Map(document.querySelector('.map'), {
+      zoom: 14,
+      center: loc
+    });
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({ position: loc, map: map });
+  }
+  
+
+  // Sticky menu background
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+      document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+      document.querySelector('#navbar').style.opacity = 1;
+    }
+  });
+
+
+  // Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  });
